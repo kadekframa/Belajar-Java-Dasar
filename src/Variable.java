6e965f171e276462677d27509bdb50e4ab17d2c8
kadekframa <dframa19@gmail.com>
2021-10-19 23:02:56 +0800

    Java Trainning Code, belajar cara penggunaan variable di Java. Tetap Semangat Berlatih Java!!! :)

--- src/Variable.java
+++ src/Variable.java
@@ -2,12 +2,19 @@ public class Variable {
     public static void main(String[] args) {
         // Variable di Java
         // Variable adalah tempat untuk menyimpan data.
-        // Java adalah bahasa static type (static languange) berbeda dengan javascript yang dinamyc type (dinamyc language), sehingga sebuah variable hanya bisa digunakan untuk menyimpan
-        //data yang sama, tidak bisa berubah-ubah tipe data seperti pada bahasa pemrograman PHP atau JavaScript.
+        // Java adalah bahasa static type (static languange) berbeda dengan javascript yang dinamyc type (dinamyc language), sehingga sebuah variable hanya bisa
+        //digunakan untuk menyimpan data yang sama, tidak bisa berubah-ubah tipe data seperti pada bahasa pemrograman PHP atau JavaScript.
         // Untuk membuat variable di Java kita bisa menggunakan nama tipe data lalu diikuti dengan nama variablenya.
         // Nama variable tidak boleh mengandung whitespace (spasi, enter, tab) dan tidak boleh seluruhnya number.
 
         // Contoh penggunaan variable pada Java.
+        String name;
+        name = "Kadek Frama Danamastyana";
+
+        int age = 21;
+        String address = "Bali, Indonesia";
+
+        System.out.println("Perkenalkan Nama saya " + name + ", umur Saya " + age + " tahun, Saya berasal dari " + address + ".");
 
     }
 }
