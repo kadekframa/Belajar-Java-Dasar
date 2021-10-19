eb73b718b2eac714e2199f552ddb1cbbd1f71e61
kadekframa <dframa19@gmail.com>
2021-10-19 22:46:02 +0800

    Java Trainning Code, belajar cara penggunaan tipe data String Java. Tetap Semangat Berlatih Java!!! :)

---
+++ src/Tipe_Data_String.java
@@ -0,0 +1,18 @@
+public class Tipe_Data_String {
+    public static void main(String[] args) {
+        // Tipe Data String Java.
+        // Tipe data String adalah tipe data yang berisikan data kumpulan karakter atau sederhananya adalah teks.
+        // Di Java, tipe data string direpresentasikan denga kata kunci String.
+        // Untuk membuat string di Java, kita menggunakan tanda "" (petik dua) yang berisikan value diantara kedua tanda petiknya.
+        // Default value untuk String adalah null.
+
+        // Contoh penggunaan tipe data String;
+        String firstName = "Kadek Frama";
+        String lastName = "Danamastyana";
+        String fullName = "Kadek Frama Danamastyana";
+
+        System.out.println("Nama depan saya adalah      : " + firstName);
+        System.out.println("Nama belakang saya adalah   : " + lastName);
+        System.out.println("My Fullname                 : " + fullName);
+    }
+}
