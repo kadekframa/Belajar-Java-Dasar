7738c113c2bedb5985d26b50a671c5bdd5f06102
kadekframa <dframa19@gmail.com>
2021-10-19 22:36:38 +0800

    Java Trainning Code, belajar cara penggunaan tipe data boolean. Tetap Semangat Berlatih Java!!!

--- src/Tipe_Data_Boolean.java
+++ src/Tipe_Data_Boolean.java
@@ -1,6 +1,16 @@
 public class Tipe_Data_Boolean {
     public static void main(String[] args) {
         // Tipe Data Boolean.
-        //
+        // Tipe data boolean adalah tipe data yang memiliki 2 nilai, yaitu benar dan salah.
+        // Nilai benar di representasikan dengan kata kunci true, sedangkan nilai salah direpresentasikan dengan kata kunci false.
+        // Default nilai untuk boolean adalah false.
+
+        //contoh penggunaan tipe data boolean.
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println(benar);
+        System.out.println(salah);
+
     }
 }
