5ba3be27095c0a31440dc62e9e0be68257629cce
kadekframa <dframa19@gmail.com>
2021-10-19 21:25:21 +0800

    Java Trainning Code, tipe data character. Tetap Semangat Berlatih Java!!! :)

---
+++ src/Tipe_Data_Character.java
@@ -0,0 +1,21 @@
+public class Tipe_Data_Character {
+    public static void main(String[] args) {
+        // Tipe data Character Java.
+        // Data character di Java direpresentasikan oleh tipe char.
+        // Untuk membuat data char di Java, kita bisa menggunakan tanda '' (petik satu).
+
+        //contoh penggunaan tipe data char.
+        char k = 'K';
+        char a = 'A';
+        char d = 'D';
+        char e = 'E';
+        char k2 = 'K';
+
+        System.out.println(k);
+        System.out.println(a);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(k2);
+
+    }
+}
