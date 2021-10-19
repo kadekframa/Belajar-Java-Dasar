ccf1c7cab73db0fa9f98ffc6e0f14cc90239ddee
kadekframa <dframa19@gmail.com>
2021-10-19 23:59:50 +0800

    Java Trainning Code, belajar cara penggunaan variable di Java. Tetap Semangat Berlatih Java!!! :)

--- src/Variable.java
+++ src/Variable.java
@@ -16,5 +16,39 @@ public class Variable {
 
         System.out.println("Perkenalkan Nama saya " + name + ", umur Saya " + age + " tahun, Saya berasal dari " + address + ".");
 
+
+
+        // Kata kunci var.
+        // Sejak versi Java 10, Java mendukun pembuatan variabel dengan kata kunci var, sehinga kita tidak perlu menyebutkan tipe datanya.
+        // Namun perlu diingat, saat kita menggunakan kata kunci var untuk membuat variabel, kita harus menginisiasi value/nilai dari variable tersebut secara langsung.
+
+        // Contoh penggunaan kata kunci var.
+//        var names;    //error
+//        names = "kadek frama";
+        var umur = 21;
+        var alamat = "Bali";
+
+        System.out.println("Umur nya Kadek  : " + umur + " tahun.");
+        System.out.println("Alamat          : " + alamat + ", Indonesia.");
+
+
+        // Kata Kunci final.
+        // secara default, variable di Java bisa diubah-ubah nilainya.
+        // Jika kita ingin membuat sebuah variable yang datanya permanen atau tidak boleh diubah setelah pertama kali dibuat, kita bisa menggunakan kata kunci "final".
+        // Istilah variabel seperti ini, banyak juga yang menyebutnya constan.
+
+        // Contoh penggunaan kata kunci final (dalam bahasa pemrograman JavaScript disebut dengan tipe data constan).
+        final String namaKadek = "Kadek Frama Danamastyana";    // Contoh penggunaan tipe data final pada Java.
+        var umurKadek = 21;
+        var alamatKadek = "Bali, Indonesia";
+
+//        namaKadek = "Kadek Frama";   //error
+
+        System.out.println("");
+        System.out.println("Tipe data final pada Java");
+        System.out.println(name);
+        System.out.println(umurKadek);
+        System.out.println(alamatKadek);
+
     }
 }
