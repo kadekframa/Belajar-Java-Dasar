b570bda1e7985b6d25d731bee21956d131fa3d72
kadekframa <dframa19@gmail.com>
2021-10-19 19:42:39 +0800

    Java Trainning Code, Tetap Semangat !!!

---
+++ src/Tipe_Data_Number.java
@@ -0,0 +1,9 @@
+public class Tipe_Data_Number {
+    public static void main(String[] args) {
+
+        byte iniByte = 100;
+        int iniInt = 1000000000;
+        System.out.println(iniByte);
+        System.out.println("Ini adalah bilangan Integer :" + iniInt);
+    }
+}
