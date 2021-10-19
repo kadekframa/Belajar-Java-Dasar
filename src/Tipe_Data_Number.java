382995ede8de3ad779bf3bd40a7e7a01bb97ccc5
kadekframa <dframa19@gmail.com>
2021-10-19 20:58:38 +0800

    Java Trainning Code, tipe data number. Tetap Semangat Berlatih Java !!!

--- src/Tipe_Data_Number.java
+++ src/Tipe_Data_Number.java
@@ -1,10 +1,20 @@
 public class Tipe_Data_Number {
     public static void main(String[] args) {
+        // Tipe data number dalam Java secara garis besar itu dibagi menjadi 2, yaitu:
+        // - integer number (bilangan bulat)
+        // - floating point number (bilangan pecahan atau desimal).
 
+        // Initialisasi variabel.
         byte iniByte = 100;
-        int iniInt = 1000000000;
-        System.out.println(iniByte);
-        System.out.println("Ini adalah bilangan Integer :" + iniInt);
-        System.out.println("Tetap Semangat !!!");
+        short iniShort = 1000;
+        int iniInt = 1000000;
+        long iniLong = 1000000000L;
+
+        // print variabel.
+        System.out.println("Ini adalah bilangan dengan tipe data byte :" + iniByte);
+        System.out.println("Ini adalah bilangan dengan tipe data short :" + iniShort);
+        System.out.println("Ini adalah bilangan dengan tipe data Integer :" + iniInt);
+        System.out.println("Ini adalah bilangan dengan tipe data long :" + iniLong);
+        System.out.println("Tetap Semangat Berlatih Java Programming Language !!!");
     }
 }
