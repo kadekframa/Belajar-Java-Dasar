a67b81a26f407855dfcc5a5fc46b5df6cf2fabc8
kadekframa <dframa19@gmail.com>
2021-10-20 23:19:09 +0800

    Java Trainning Code, Operator Perbandingan di Java. Tetap Semangat Berlatih Java!!!

---
+++ src/Operasi_Perbandingan.java
@@ -0,0 +1,20 @@
+public class Operasi_Perbandingan {
+    public static void main(String[] args) {
+        // Operasi Perbandingan di Java (>, <, >=, <=, ==, !=).
+        // Operasi perbandingan adalah operasi untuk membandingankan dua buah data.
+        // Operasi perbandingan adalah operasi yang menghasilkan nilai boolean (benar atau salah).
+        // Jika hasil operasinya adalah benar, maka nilainya adalah true, jika hasil operasinya adalah salah, maka nilainya adalah false.
+
+        // Contoh penggunaan operasi perbandingan di Java.
+        int value1 = 100;
+        int value2 = 94;
+
+        System.out.println(value1 < value2);
+        System.out.println(value1 > value2);
+        System.out.println(value1 <= value2);
+        System.out.println(value1 >= value2);
+        System.out.println(value1 == value2);
+        System.out.println(value1 != value2);
+
+    }
+}
