29c4438a16955aae5af1ce63ec528e0076dcf646
kadekframa <dframa19@gmail.com>
2021-10-20 12:45:37 +0800

    Java Trainning Code, belajar cara penggunaan tipe data array di Java. Tetap Semangat Berlatih Java!

---
+++ src/Tipe_Data_Array.java
@@ -0,0 +1,10 @@
+public class Tipe_Data_Array {
+    public static void main(String[] args) {
+        // Tipe Data Array Java.
+        // Array adalah tipe data yang berisikan kumpulan tipe data yang sama.
+        // Jumlah data di Array tidak bisa berubah setelah pertama kali dibuat.
+
+        // Contoh penggunaan Array.
+
+    }
+}
