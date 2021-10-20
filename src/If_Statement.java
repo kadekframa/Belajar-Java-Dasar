54c21ac857ef524724918cce9339b28b8b718b8d
kadekframa <dframa19@gmail.com>
2021-10-21 02:37:15 +0800

    Java Trainning Code, belajar if statement di Java. Semangat!

---
+++ src/If_Statement.java
@@ -0,0 +1,19 @@
+public class If_Statement {
+    public static void main(String[] args) {
+        // If statement di Java.
+        // Dalam Java, if adalah salah satu kata kunci yang digunakan untuk percabangan.
+        // Percabangan artinya kita bisa mengeksekusi kode program tertentu ketika suatu kondisi terpenuhi.
+        // Hampir di semua bahasa pemrograman mendukung if expression.
+
+        // Contoh penggunaan if statement di Java.
+        var nilai = 70;
+        var absen = 90;
+
+        if (nilai >= 75 && absen >= 75){
+            System.out.println("Anda Lulus");
+        }else {
+            System.out.println("Harus Tetap Semangat!");
+        }
+
+    }
+}
