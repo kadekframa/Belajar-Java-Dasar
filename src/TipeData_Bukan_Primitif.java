d1369a0e1fc4c53102eda6dd83d13c944dea445a
kadekframa <dframa19@gmail.com>
2021-10-20 12:33:06 +0800

    Java Trainning Code, belajar cara penggunaan tipe data bukan primitif di Java. Tetap Semangat Berlatih Java!!!

--- src/TipeData_Bukan_Primitif.java
+++ src/TipeData_Bukan_Primitif.java
@@ -18,5 +18,29 @@ public class TipeData_Bukan_Primitif {
 
                 iniByte = 100;                  // Tipe data primitif.
                 System.out.println(iniByte);
+
+
+                // Konversi tipe data primitif ke tipe data bukan primitif.
+                int iniInt = 240;
+                Integer iniInteger2 = iniInt;   // contoh cara konversi dari tipe data primitif ke bukan primitif.
+
+                System.out.println(iniInteger2);
+
+
+                // Konversi tipe data bukan primitif (object) ke tipe data primitif.
+                // contoh 1
+                Integer ageObject = 21;
+                int ageprimitf = ageObject;     // konversi tipe data bukan primitif ke tipe data primitif.
+
+                System.out.println("");
+                System.out.println("Ini adalah contoh cara konversi dari tipe data bukan primitif ke tipe data primitif. " + ageprimitf);
+
+                // contoh 2
+                Integer ageObjectAgain = 21;
+                short shortAge = ageObjectAgain.shortValue();        // cara konversi tipe data bukan primitif ke tipe data primitf namun berbeda tipe data (dalam hal ini dikonversi dari Integer ke short).
+                byte byteAge = ageObjectAgain.byteValue();           // cara konversi tipe data bukan primitif ke tipe data primitf namun berbeda tipe data (dalam hal ini dikonversi dari Integer ke byte).
+                System.out.println("");
+                System.out.println("Ini adalah contoh cara konversi dari tipe data bukan primitif ke tipe data primitif yang tidak compatible (dalam artian dari Integer dikonversi ke short). " + shortAge);
+                System.out.println("Ini adalah contoh cara konversi dari tipe data bukan primitif (Integer) ke tipe data primitif (byte) (dalam hal ini dari Integer dikonversi ke byte). " + byteAge);
         }
 }
