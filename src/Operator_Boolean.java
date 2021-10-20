20197683cc453adfc3eafc18bd510545cfad8234
kadekframa <dframa19@gmail.com>
2021-10-20 23:33:03 +0800

    Java Trainning Code, belajar penggunaan Operasi Boolean di Java. Tetap Semangat Berlatih Java!!!

---
+++ src/Operator_Boolean.java
@@ -0,0 +1,23 @@
+public class Operator_Boolean {
+    public static void main(String[] args) {
+        // Operasi Boolean di Java (&&, ||, !).
+        // Operasi boolean adalah operasi yang digunakan untuk dua data boolean.
+
+        // Contoh Penggunaan Operasi Boolean.
+        var absen = 70;
+        var nilaiAkhir = 80;
+
+        var lulusAbsen = absen >= 75;
+        var lulusNilaiAkhir = nilaiAkhir >= 74;
+
+        var lulus = lulusAbsen && lulusNilaiAkhir;                  // Operator && (AND)
+        var lulusTetapSemangat = lulusAbsen || lulusNilaiAkhir;     // Operator || (OR)
+        System.out.println("Hasil Kelulusan: " + lulus);
+        System.out.println("Hasil Kelulusan Bersyarat: " + lulusTetapSemangat);
+
+        if (lulusTetapSemangat == true){
+            System.out.println("Semangat Menggapai Mimpi!");
+        }
+
+    }
+}
