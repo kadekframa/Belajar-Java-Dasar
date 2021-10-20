d8ee16bfb6de54287db9e16a9166d47760e74f97
kadekframa <dframa19@gmail.com>
2021-10-20 21:15:03 +0800

    Java Trainning Code, cara penggunaan operasi matematika di Java. Tetap Semangat Berlatih Java!

---
+++ src/Operasi_Matematika.java
@@ -0,0 +1,16 @@
+public class Operasi_Matematika {
+    public static void main(String[] args) {
+        // Operasi Matematika di Java.
+
+        //Contoh penggunaan Operasi Matematika di Java.
+        int a = 100;
+        int b = 10;
+        int kurang = a - b;
+
+        System.out.println("a + b = " + a + b);
+        System.out.println("a - b = " + kurang);
+        System.out.println("a * b = " + a * b);
+        System.out.println("a / b = " + a / b);
+        System.out.println("a % b = " + a % b);
+    }
+}
