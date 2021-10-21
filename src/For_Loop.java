abec703efdd7b52d793f67de348be1687c505f18
kadekframa <dframa19@gmail.com>
2021-10-21 22:00:07 +0800

    Java Trainning Code, belajar For Loop di Java. Tetap Semangat!

---
+++ src/For_Loop.java
@@ -0,0 +1,11 @@
+public class For_Loop {
+    public static void main(String[] args) {
+        // For loop di Java.
+        // For adalah salah satu kata kunci yang bisa digunakan untuk melakukan perulangan.
+        // Blok kode yang terdapat di dalam for akan selalu diulangi selama kondisi for terpenuhi.
+
+        // Contoh penggunaan perulangan for di Java.
+
+
+    }
+}
