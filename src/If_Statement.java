e2e52b0b6b5e98bd1c5d3eceff97963c697d2471
kadekframa <dframa19@gmail.com>
2021-10-21 09:35:22 +0800

    Java Trainning Code, belajar if dan else if statement di Java. Semangat!!!

--- src/If_Statement.java
+++ src/If_Statement.java
@@ -15,5 +15,19 @@ public class If_Statement {
             System.out.println("Harus Tetap Semangat!");
         }
 
+
+        // Contoh penggunaan if, else if, else.
+        if (nilai >= 80 && absen >= 80){
+            System.out.println("Nilai Anda A");
+        }else if(nilai >= 70 && absen >= 70){
+            System.out.println("Nilai Anda B");
+        }else if(nilai >= 60 && absen >= 60){
+            System.out.println("Nilai Anda C");
+        }else if(nilai >= 50 && absen >= 50){
+            System.out.println("Nilai Anda D");
+        }else{
+            System.out.println("Nilai Anda E");
+        }
+
     }
 }
