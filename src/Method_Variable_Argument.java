3c2f7f464ad2a3d34acbb53a8753daec2c5d0381
kadekframa <dframa19@gmail.com>
2021-10-23 02:11:36 +0800

    Java Trainning Code, belajar penggunaan method variable argument di Java. Semangat!

---
+++ src/Method_Variable_Argument.java
@@ -0,0 +1,13 @@
+public class Method_Variable_Argument {
+    public static void main(String[] args) {
+        // Method variable argument.
+        // Kadang kita butuh mengirim data ke method sejumlah data yang tidak pasti.
+        // Biasanya, agar bisa seperti ini, kita akan menggunakan array sebagai parameter di method tersebut,
+        // Namun di Java, kita bisa menggunakan variable argument, untuk mengirim data yang berisi jumlah yak tentu, bisa nol atau lebih.
+        // Parameter dengan tipe variable argument, hanya bisa ditempatkan di posisi akhir parameter.
+
+        // Contoh pennggunaan method variable argument.
+
+
+    }
+}
