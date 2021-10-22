08e6c5310ec275a6903c2932ec07a51cf0c1bab9
kadekframa <dframa19@gmail.com>
2021-10-22 23:18:44 +0800

    Update Java Trainning Code, belajar penggunaan for each di Java. Tetap Semangat!!! :)

--- src/For_Each.java
+++ src/For_Each.java
@@ -7,6 +7,30 @@ public class For_Each {
 
         // Contoh penggunaan for each di Java.
 
+        // Pemanggilan array dengan perulangan tanpa menggunakan for each.
+        String[] array = {
+                "Kadek", "Frama", "Danamastyana", "Programmer", "Zaman", "Now"
+        };
+        System.out.println("");
+        System.out.println("perulangan array tanpa for each");
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+
+
+
+        // Contoh penggunaan perulangan dengan for each.
+        String[] arrayForEach = {
+                "Kadek", "Frama", "Danamastyana", "Programmer", "Tetap", "Semangat"
+        };
+
+        System.out.println("");
+        System.out.println("perulangan array dengan menggunakan for each");
+        for (var value : arrayForEach){                                             // penggunaan perulangan for each. Digunakan untuk mengambil data array satu per satu.
+            System.out.println(value);
+        }
+
+
 
 
     }
