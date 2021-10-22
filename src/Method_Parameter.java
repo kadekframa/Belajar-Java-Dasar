2201ee6c34de5439fb04a6b4168de9d506db088d
kadekframa <dframa19@gmail.com>
2021-10-22 23:49:29 +0800

    Java Trainning Code, belajar penggunaan method  parameter di Java. Tetap Semangat!

---
+++ src/Method_Parameter.java
@@ -0,0 +1,13 @@
+public class Method_Parameter {
+    public static void main(String[] args) {
+        // Method Parameter
+        // Kita bisa mengirimkan informasi ke method yang kita panggil.
+        // Untuk melakukan hal tersebut, kita perlu menambahkan parameter atau argument di method yang sudah kita buat.
+        // Cara membua parameter sama seperti cara membuat variabel.
+        // Parameter ditempatkan di dalam kurung () di deklarasi method.
+        // Parameter bisa lebih dari satu, jika lebih dari satu, harus dipisah menggunakan tanda koma.
+
+        // Contoh penggunaan method parameter di Java.
+
+    }
+}
