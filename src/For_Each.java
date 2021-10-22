435e94929d7cf613657f92d2987499be75beba7f
kadekframa <dframa19@gmail.com>
2021-10-22 22:42:33 +0800

    Java Trainning Code, belajar penggunaan for each di Java. Tetap Semangat! :)

---
+++ src/For_Each.java
@@ -0,0 +1,13 @@
+public class For_Each {
+    public static void main(String[] args) {
+        // For each di Java
+        // Kadang kala kita biasa mengakses data array menggunakan perulangan.
+        // Mengakses data array menggunakan perulangan sangat bertele-tele, kita harus membuat counter, lalu mengakses array menggunakan counter yang kita buat.
+        // Namun untungnya, di java terdapat perulangan for each yang bisa digunakan untuk mengakses seluruh data di array secara otomatis.
+
+        // Contoh penggunaan for each di Java.
+
+
+
+    }
+}
