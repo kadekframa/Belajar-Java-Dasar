e4fb8e591b42eef928b490ca78c5b23bb609e647
kadekframa <dframa19@gmail.com>
2021-10-22 23:39:48 +0800

    Java Trainning Code, belajar penggunaan method di Java. Tetap Semangat! :)

---
+++ src/Method.java
@@ -0,0 +1,14 @@
+public class Method {
+    public static void main(String[] args) {
+        // Method atau Function.
+        // Method adalah block kode program yang akan berjalan saat kita panggil.
+        // Sebelumnya kita sudah menggunaakan method println() untuk menampilkan tulisan di console.
+        // Untuk membuat method di Java, kita bisa menggunakan kata kunci void, lalu diikuti dengan nama method, kurung () dan diakhiri dengan block.
+        // Kita bisa memanggil method dengan menggunakan nama method lalu diikuti dengan kurung ().
+        // Di bahasa pemrograman lain, method juga disebut dengan function.
+
+        // Contoh penggunaan method
+        
+
+    }
+}
