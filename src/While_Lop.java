7c246f670f64ad36e8265435edd941a565bfcf76
kadekframa <dframa19@gmail.com>
2021-10-22 12:14:25 +0800

    Java Trainning Code, belajar While Loop di Java. Tetap Semangat!

---
+++ src/While_Lop.java
@@ -0,0 +1,18 @@
+public class While_Lop {
+    public static void main(String[] args) {
+        // While Loop di Java.
+        // While loop adalah versi perulangan yang lebih sederhana dibanding for loop.
+        // di while loop, hanya terdapat kondisi perulangan, tanpa ada init statement dan post statement.
+
+        // Contoh penggunaan while loop di Java.
+        var counter = 1;
+
+        while (counter <= 10){
+            System.out.println("Perulangan ke-" + counter);
+            counter++;
+        };
+
+
+
+    }
+}
