cb2df6b91bfa17b864c48938ee468dfc306811a2
kadekframa <dframa19@gmail.com>
2021-10-22 22:30:24 +0800

    Update Java Trainning Code, belajar penggunaan break dan continue di Java. Tetap Semangat! :)

--- src/Break_Dan_Continue.java
+++ src/Break_Dan_Continue.java
@@ -13,10 +13,22 @@ public class Break_Dan_Continue {
             counter++;
 
             if (counter > 10){
-                break;              // break yang digunakan untuk menghentikan perulangan.
+                break;                                                                  // break yang digunakan untuk menghentikan perulangan.
             }
-
-
         }
+        System.out.println("Perulangan berhenti oleh break.");
+        System.out.println("");
+
+
+
+        // Contoh penggunaan continue di Java.
+        for (int counterContinue = 1; counterContinue <= 34; counterContinue++){
+            if (counterContinue % 2 == 0){
+                continue;                                                                // continue artinya kode yang dibawahnya tidak akan dieksekusi, dan akan dilanjutkan ke perulangan selanjutnya.
+            }
+            System.out.println("Perulangan ganjil kurang dari 34, yaitu: " + counterContinue);
+        }
+
+
     }
 }
